7e82807793270b3a4a92c7c2b7d10fbd0a9c2cfa
nevalhr <phneval@gmail.com>
2026-02-04 19:28:45 +0700

    feat:structure and file system

---
+++ internal/domain/movie.go
@@ -0,0 +1,8 @@
+package domain
+
+type Movie struct {
+	Title       string  `json:"title"`
+	Overview    string  `json:"overview"`
+	ReleaseDate string  `json:"release_date"`
+	VoteAverage float64 `json:"vote_average"`
+}
