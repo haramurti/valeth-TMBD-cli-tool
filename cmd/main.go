4c66294bec9e2ba20a875ffa6c7528bf094df5d4
nevalhr <phneval@gmail.com>
2026-02-07 20:03:19 +0700

    feat : phase 3 CLI and Flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	movieType := flag.String("type", "popular", "Pilih tipe film: playing, popular, top, upcoming")
+
+	flag.Parse()
+
+	switch *movieType {
+	case "playing", "popular", "top", "upcoming":
+		fmt.Println("Siap! Sedang mengambil data film kategori:", *movieType)
+	default:
+		fmt.Println("❌ Error: Tipe film tidak dikenal!")
+		fmt.Println("Tolong pilih salah satu: playing, popular, top, upcoming")
+
+		os.Exit(1)
+	}
 
 }
